a7bf1aed2349ffdcb084aeb97519ce9056c7c878
szuryuu <ilhamdzaky2007@gmail.com>
2026-07-17 17:44:39 +0700

    chore: delete custom icon collection and empty stylesheet
    Remove assets/images/icons/ directory and empty assets/css/text-custom.css.

--- assets/images/icons/index.ts
+++
@@ -1,8 +0,0 @@
-import Github from "./github.svg";
-import Github2 from "./github2.svg";
-import Mail from "./gmail.svg";
-import Linkedin from "./linkedin.svg";
-import FileText from "./file-text.svg";
-import Pen from "./pen.svg";
-
-export { Github, Github2, Mail, Linkedin, FileText, Pen };
