fb3e78c9b0916166bc0dfce29d1847697bf5949d
szuryuu <ilhamdzaky2007@gmail.com>
2026-07-17 17:44:43 +0700

    chore: delete unused Prose content components
    Remove ProseImg, ProsePre, and ProseTable components.

--- components/content/ProseTable.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="w-full overflow-x-auto whitespace-nowrap md:whitespace-normal pb-4 my-6">
-    <table class="w-full text-left border-collapse">
-      <slot />
-    </table>
-  </div>
-</template>
